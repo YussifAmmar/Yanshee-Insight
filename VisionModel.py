78485d855af255a6e941c36939519db246396210
MostafaEwis <mostafanow123@gmail.com>
2025-07-29 14:44:55 +0300

    removed a variable from the loop

--- VisionModel.py
+++ VisionModel.py
@@ -1,7 +1,12 @@
+import asyncio
+import time
 import torch
 import cv2
 from ultralytics import YOLO
-import YanAPI 
+import YanAPI
+import warnings
+warnings.filterwarnings('ignore')
+
 yanip = 12
 print("starting")
 YanAPI.yan_api_init(f"192.168.1.{yanip}")
@@ -12,8 +17,12 @@ print("Camera stream started...")
 stream_url = f"http://192.168.1.{yanip}:8000/stream.mjpg" 
 cap = cv2.VideoCapture(stream_url)
 
+#the event loop creation
+#loop = asyncio.get_event_loop()
+#change_scene_task = loop.create_task()
 
 model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
+saw_person = False
 while cap.isOpened():
     ret, frame = cap.read()
     if not ret:
@@ -21,33 +30,33 @@ while cap.isOpened():
         break
 
     results = model(frame)
-    #say objects names outloud
-    saw_person = False
+    results.render()
+    annotated_frame = results.ims[0]
+    cv2.imshow("Yanshee Object Detection", annotated_frame)
+   #say objects names outloud
     names = results.pandas().xyxy[0]
     kotsy = names['name'].tolist()
-    for i in kotsy:
-        print(i)
-        if "person" in kotsy:
-            saw_person = True 
-        YanAPI.sync_do_tts(results.pandas().xyxy[i].name)
+    #for i in kotsy:
+    #print(i)
+    if "person" in kotsy:
+        saw_person = True 
+    #YanAPI.sync_do_tts(kotsy)
 
     if saw_person: 
         vis_task_res = YanAPI.get_visual_task_result("face", "recognition")
-        if vis_task_res.data.recognition.name == "":
+        if vis_task_res["data"]["recognition"]["name"] == "none":
             YanAPI.set_robot_led("camera", "red", "on")
             YanAPI.set_robot_led("button", "red", "on")
         else: 
             YanAPI.set_robot_led("camera", "green", "on")
             YanAPI.set_robot_led("button", "green", "on")
+            YanAPI.start_play_motion("bow")
     else:
             YanAPI.set_robot_led("camera", "blue", "on")
             YanAPI.set_robot_led("button", "blue", "on")
-    results.render()
-    annotated_frame = results.ims[0]
-
-    cv2.imshow("Yanshee Object Detection", annotated_frame)
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
+    #time.sleep(0.1)
 
 
 cap.release()
