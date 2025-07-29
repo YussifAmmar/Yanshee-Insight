c9f2ed152c49c5515b6c0b68b51fceb244e8e270
MostafaEwis <mostafanow123@gmail.com>
2025-07-29 13:19:27 +0300

    initial commit

--- VisionModel.py
+++ VisionModel.py
@@ -1,18 +1,19 @@
+import torch
 import cv2
 from ultralytics import YOLO
 import YanAPI 
-
-YanAPI.yan_api_init("192.168.1.24")
-
-response = YanAPI.open_vision_stream(resolution="640x480")
+yanip = 12
+print("starting")
+YanAPI.yan_api_init(f"192.168.1.{yanip}")
+print("intialized ip")
+response = YanAPI.open_vision_stream()
 print("Camera stream started...")
 
-stream_url = "http://192.168.1.24:8000/stream.mjpg" 
+stream_url = f"http://192.168.1.{yanip}:8000/stream.mjpg" 
 cap = cv2.VideoCapture(stream_url)
 
 
-model = YOLO("yolov5x-cls.pt")
-
+model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
 while cap.isOpened():
     ret, frame = cap.read()
     if not ret:
@@ -20,7 +21,29 @@ while cap.isOpened():
         break
 
     results = model(frame)
-    annotated_frame = results[0].plot()
+    #say objects names outloud
+    saw_person = False
+    names = results.pandas().xyxy[0]
+    kotsy = names['name'].tolist()
+    for i in kotsy:
+        print(i)
+        if "person" in kotsy:
+            saw_person = True 
+        YanAPI.sync_do_tts(results.pandas().xyxy[i].name)
+
+    if saw_person: 
+        vis_task_res = YanAPI.get_visual_task_result("face", "recognition")
+        if vis_task_res.data.recognition.name == "":
+            YanAPI.set_robot_led("camera", "red", "on")
+            YanAPI.set_robot_led("button", "red", "on")
+        else: 
+            YanAPI.set_robot_led("camera", "green", "on")
+            YanAPI.set_robot_led("button", "green", "on")
+    else:
+            YanAPI.set_robot_led("camera", "blue", "on")
+            YanAPI.set_robot_led("button", "blue", "on")
+    results.render()
+    annotated_frame = results.ims[0]
 
     cv2.imshow("Yanshee Object Detection", annotated_frame)
     if cv2.waitKey(1) & 0xFF == ord("q"):
@@ -31,18 +54,3 @@ cap.release()
 cv2.destroyAllWindows()
 YanAPI.close_vision_stream()
 
-
-
-'''
-If I wanna test on taken image ? 
-use this command -> results = model("image.jpg")
-annotated_frame = results[0].plot()
-cv2.imshow("Yanshee Vision - YOLO Detection", annotated_frame) 
-
-wanna test on a folder of images ? 
-add '*' before 'jpg'
-
-wanna test on taken vid ? 
-cap = cv2.VideoCapture("vid.mp4")
-
-'''
