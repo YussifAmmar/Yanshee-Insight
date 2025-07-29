73287fab42e500a61d389520076934d002e01ff8
yussif <youssefammar390@gmail.com>
2025-07-29 05:10:41 +0300

    Updating VisionModel

--- VisionModel.py
+++ VisionModel.py
@@ -1,27 +1,36 @@
 import cv2
 from ultralytics import YOLO
+import YanAPI 
+
+YanAPI.yan_api_init("192.168.1.24")
+
+response = YanAPI.open_vision_stream(resolution="640x480")
+print("Camera stream started...")
+
+stream_url = "http://192.168.1.24:8000/stream.mjpg" 
+cap = cv2.VideoCapture(stream_url)
+
 
 model = YOLO("yolov5x-cls.pt")
-
-# Replace with Yanshee's stream URL
-stream_url = "http://192.168.1.24:8080/video"
-cap = cv2.VideoCapture(stream_url)
 
 while cap.isOpened():
     ret, frame = cap.read()
     if not ret:
-        print("Stream not available.")
+        print("Failed to get frame")
         break
 
     results = model(frame)
     annotated_frame = results[0].plot()
 
-    cv2.imshow("Yanshee IP YOLO", annotated_frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    cv2.imshow("Yanshee Object Detection", annotated_frame)
+    if cv2.waitKey(1) & 0xFF == ord("q"):
         break
+
 
 cap.release()
 cv2.destroyAllWindows()
+YanAPI.close_vision_stream()
+
 
 
 '''
